ea23a684ff320835d1be11d0de77ab3eac3c9296
Emmanuelcookey15 <Emmanuelcookey744@gmail.com@users.noreply.github.com>
2020-07-14 02:46:40 +0100

    Building additional pages like topic pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all topics
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single Page
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
