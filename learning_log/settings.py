125c017dc24f83d7c5f48b8f6bd69fcfd1416be8
Emmanuelcookey15 <Emmanuelcookey744@gmail.com@users.noreply.github.com>
2020-07-25 06:29:22 +0100

    securing Secret key in settings.py file

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -16,11 +16,24 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+# Handling Key Import Errors
+def get_env_variable(var_name):
+    """ Get the environment variable or return exception """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+# Get ENV VARIABLES key
+ENV_ROLE = get_env_variable('ENV_ROLE')
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'or-ny3w$qkl#&p+etz8ie_f^9me2a*)0&j1r7tflb(mw^i6wmq'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
